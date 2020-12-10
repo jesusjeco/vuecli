d469441564285070b2af30ee891ac3cbc8c212c0
Jesus Carrero <jesusenrique.carrero@gmail.com>
2020-12-10 00:13:30 -0400

    First commit

---
+++ src/components/MyFirstVueComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        This is my first vue component
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyFirstVueComponent',
+
+    }
+</script>
+
+<style scoped>
+
+</style>
