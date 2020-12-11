2967c6110e28218359d710648ab70c540cb19ea9
Jesus Carrero <jesusenrique.carrero@gmail.com>
2020-12-11 02:18:06 -0400

    Axios added
    Axios installed
    Axios view created
    Axios component for testing created
    A test with axios get function done successful.

---
+++ src/components/AxiosComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    And this is the axios component
+    <div class="container">
+      <div class="row">
+        <div class="col-12">Call status: {{ AxiosCall }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "AxiosComponent",
+  data() {
+    return {
+      AxiosCall: "",
+    };
+  },
+  methods: {
+    UpdateAxiosCall: function () {
+      this.AxiosCall = "Actualizado";
+      console.log("Hola");
+    },
+  },
+  created() {},
+  mounted() {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts", {
+        params: {
+          //ID: 12345
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        this.AxiosCall = "updated";
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Axios.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    This is the Axios View
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <AxiosComponent></AxiosComponent>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AxiosComponent from "../components/AxiosComponent";
+export default {
+  name: "Axios",
+  components: {
+    AxiosComponent,
+  },
+};
+</script>
+
+<style scoped>
+</style>
